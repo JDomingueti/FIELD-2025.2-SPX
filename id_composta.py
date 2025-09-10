4e90e77df96ef1c303c35f0cc94ce8a2b2b431b0
Sofia <sofia.moumont@gmail.com>
2025-09-10 20:05:14 -0300

    Salvamento do arquivo com coluna de id com sobrescrição

--- id_composta.py
+++ id_composta.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from to_parquet import make_data_paths
 
-def id_composta(trimestre):
-    df = pd.read_parquet(trimestre)
+def id_composta(file):
+    df = pd.read_parquet(file)
 
     df["person_id"] = (
         df["UF"].astype(str).str.zfill(2) +
@@ -13,3 +13,14 @@ def id_composta(trimestre):
         )
     
     return df
+
+if __name__ == "__main__":
+    y, t = input("Ano desejado: "), input("Trimestre desejado: ")
+    
+    _, file = make_data_paths(y, t)
+    
+    df = id_composta(file)
+    
+    df.to_parquet(file, index=False)
+    
+    
