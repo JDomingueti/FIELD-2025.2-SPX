a842c489888b4a32eab503d0a93c89f6a83ff026
quintellabernardo <bvquintella@gmail.com>
2025-10-08 10:59:53 -0300

    adicionando funcao de calculo da mediana da renda para 100% dos individuos classificados de 1 a 3 naquele trimestre

--- utils.R
+++ utils.R
@@ -30,3 +30,38 @@ shift_quarter <- function(year, tri, n) {
   new_tri <- (q_abs %% 4) + 1
   return(list(year = new_year, tri = new_tri))
 }
+
+# ====== FUNCAO PARA PEGAR A MEDIANA DA RENDA DE UM TRI E ANO PARA 100% DOS INDIVIDUOS ======
+catch_median_renda <- function(y, t) {
+  # gerar lista de tris (alvo + 4 tri anteriores)
+  quarters <- lapply(0:4, function(i) shift_quarter(y, t, -i))
+  
+  # ler os 5 parquets correspondentes
+  dfs <- lapply(quarters, function(q) {
+    path <- here("PNAD_data", q$year, paste0("PNADC_0", q$tri, q$year, ".parquet"))
+    if (file.exists(path)) {
+      read_parquet(path) %>%
+      #filtrar apenas para o ano (y) e trimestre (t)
+        filter(Ano == y, Trimestre == t, classe_individuo %in% 1:3) %>%
+        mutate(ano_arquivo = q$year, tri_arquivo = q$tri)
+      
+    } else {
+      warning(pate("Arquivo nao encontrado:", path))
+      NULL
+    }
+  })
+  # caso algum arquivo nao exista
+  dfs <- Filter(Negate(is.null), dfs)
+  
+  # concatenar os dados
+  dados <- bind_rows(dfs)
+  
+  # selecionar cols relevantes
+  dados_sel <- dados %>%
+    select(ID_UNICO, VD4020, Ano, Trimestre) # renda efetiva total
+  
+  # calcular a mediana
+  mediana_renda <- median(dados_sel$VD4020, na.rm = TRUE)
+  
+  return(mediana_renda)
+}
