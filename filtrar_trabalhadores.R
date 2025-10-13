f661ba9ffc6c3e7647c167ebcddeccdd5e641de7
quintellabernardo <bvquintella@gmail.com>
2025-10-13 12:44:53 -0300

    adicionando cols necessarias e criando job_swicther

--- filtrar_trabalhadores.R
+++ filtrar_trabalhadores.R
@@ -22,5 +22,34 @@ classificar_trabalhador_app <- function(df) {
 
 # ===== FUNCAO PARA ACHAR QUEM SAO JOB SWITCHERS ENTRE OS TRABALHADORES ====== 
 filtrar_job_switcher <- function(df) {
+  df <- df %>%
+    arrange(ID_UNICO, Ano, Trimestre) %>%
+    group_by(ID_UNICO) %>%
+    mutate(
+      VD4001_lag = lag(VD4001),
+      V4010_lag = lag(V4010),
+      V4013_lag = lag(V4013),
+      VD4009_lag = lag(VD4009),
+      V4040_lag = lag(V4040),
+      
+      # Criar col job_switch_event
+      job_switch_event = case_when(
+        # Caso 1 -> segue ocupado e mudou ocupacao/setor/posicao
+        VD4001 == 1 & VD4001_lag == 1 &
+          (V4010 != V4010_lag | V4013 != V4013_lag | VD4009 != VD4009_lag) ~ 1,
+        
+        # Caso 2 -> mesma ocupacao/setor/posicao , mas mudou emprego (tempo de emprego zerou)
+        VD4001 == 1 & VD4001_lag == 1 &
+          V4010 == V4010_lag & V4013 == V4013_lag & VD4009 == VD4009_lag &
+          V4040_lag %in% c(2, 3, 4) & V4040 == 1 ~ 1,
+        
+        # C.C
+        TRUE ~ 0
+      )
+    ) %>%
+    # Se trocou de emprego em algum dos 5 trimestres
+    mutate(job_switcher = as.integer(max(job_switch_event))) %>%
+    ungroup()
   
+  return(df)
 }
