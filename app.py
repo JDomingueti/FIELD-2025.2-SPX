779104f57fad412c3de0be92c49cb3c86787a264
sofiaazeredo <sofia.moumont@gmail.com>
2025-10-23 11:56:48 -0300

    Adicionando os filtros

--- app.py
+++ app.py
@@ -4,9 +4,49 @@ import altair as alt
 
 st.set_page_config(page_title="Wage Tracker", layout="wide")
 
-df = pd.read_csv("../medianas_variacao_renda_2012_2017.csv")
+filtros = {
+    "Base": "_0",
+    "Trabalhador de App": "_1",
+    "Job Switcher": "_2"
+}
 
-years = sorted(df["ano_inicial"].unique())
+deflator = {
+    "Não": "",
+    "Sim": "D"
+}
+
+arquivo_base = "./dados_medianas_var/medianas_variacao_renda"
+
+@st.cache_data
+def load_data(suffix):
+    """Carrega o DataFrame com base no sufixo do filtro."""
+    file_path = f"{arquivo_base}{suffix}.csv"
+    df = pd.read_csv(file_path)
+    return df
+
+st.sidebar.title("Opções de Filtro")
+
+grupo_selecionado = st.sidebar.radio(
+    "Grupo Específico",
+    options=list(filtros.keys()),
+    index=0
+)
+
+deflator_selecionado = st.sidebar.radio(
+    "Deflator Aplicado",
+    options=list(deflator.keys()),
+    index=0 
+)
+
+codigo_grupo = filtros[grupo_selecionado]
+
+codigo_deflator = deflator[deflator_selecionado]
+
+file_suffix = codigo_grupo + codigo_deflator
+
+df = load_data(file_suffix)
+
+years = sorted(df["ano_final"].unique())
 year_range = st.slider(
     "Anos de Range",
     min_value=int(min(years)),
@@ -14,8 +54,8 @@ year_range = st.slider(
     value=(int(min(years)), int(max(years)))
 )
 
-filtered = df[(df["ano_inicial"] >= year_range[0]) & (df["ano_inicial"] <= year_range[1])]
-filtered["periodo"] = filtered['ano_inicial'].astype(str) + '.' + filtered['trimestre'].astype(str)
+filtered = df[(df["ano_final"] >= year_range[0]) & (df["ano_final"] <= year_range[1])]
+filtered["periodo"] = filtered['ano_final'].astype(str) + '.' + filtered['trimestre'].astype(str)
 
 chart = (
     alt.Chart(filtered)
