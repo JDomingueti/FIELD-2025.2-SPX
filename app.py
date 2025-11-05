61d84faac7874d19c6ee26008166ae7e6ebc8e70
JDomingueti <jean.domingueti@gmail.com>
2025-11-05 00:21:50 -0300

    Modificando filtros de ocupações e adicionando feature de seleção

--- app.py
+++ app.py
@@ -25,11 +25,19 @@ grupos_suffix = {
     "14-24 anos": "_14",
     "25-54 anos": "_15",
     "55+ anos": "_16",
-    "Lideres e militares": "_191",
-    "Técnicos e Profissionais": "_192",
-    "Indústria/Comércio/Serviços": "_193",
-    "Trabalhadores qualificados": "_194",
-    "Ocupações Elementares": "_195"
+    "Diretores e gerentes":"_191",
+    "Prof. das ciências e intelectuais":"_192",
+    "Prof. de nível médio":"_193",
+    "Trab. de apoio adm.":"_194",
+    "Trab. de serv. e vend.":"_195",
+    "Trab. ambientais qualificados":"_196",
+    "Trab. urbanos qualificados":"_197",
+    "Operadores de máquinas":"_198",
+    "Ocupações elementares":"_199",
+    "Militares":"_190",
+    "Comércios": "_201",
+    "Serviços": "_202",
+    "Indústrias": "_203"
 }
 
 # Mapeia a opção do deflator para o sufixo
@@ -103,7 +111,7 @@ def create_combined_chart(df, group_column=None):
     )
 
     # --- Define as configurações de cor e tooltip ---
-    color_encoding = alt.Undefined
+    color_encoding = alt.Color()
     tooltip_list = [
         'periodo', 
         alt.Tooltip('mediana_variacao', format='.1%'), 
@@ -114,12 +122,23 @@ def create_combined_chart(df, group_column=None):
         # Se um group_column é dado (ex: "Grupo"), usa para colorir as linhas
         color_encoding = alt.Color(f"{group_column}:N", title="Grupo")
         tooltip_list.append(alt.Tooltip(f"{group_column}:N", title="Grupo"))
+        selection = alt.selection_point(
+            fields=["Grupo"],   # As categorias que queremos permitir a interação
+            bind="legend",      # Liga a interação com a legenda
+        )
+        opacidade = alt.condition(selection, alt.value(1), alt.value(0.1))
+        p = [selection]
+    else:
+        opacidade = alt.value(1)
+        p = []
 
     # Gráfico base
     base = alt.Chart(filtered_df).encode(
         x=alt.X("periodo:O", title="Ano.Trimestre"),
-        tooltip=tooltip_list
-    )
+        color="Grupo:N",
+        tooltip=tooltip_list,
+        opacity=opacidade
+    ).add_params(*p)
 
     # Gráfico 1: Variação da Renda
     chart_renda = base.mark_line().encode(
@@ -154,7 +173,7 @@ def create_combined_chart(df, group_column=None):
 st.title("Mediana da Variação da Renda")
 
 # Cria as abas
-tab_base, tab_app, tab_switcher, tab_sexo, tab_regioes, tab_carteira, tab_quartis, tab_idade, tab_ocp = st.tabs([
+tab_base, tab_app, tab_switcher, tab_sexo, tab_regioes, tab_carteira, tab_quartis, tab_idade, tab_ocp, tab_div = st.tabs([
     "Base", 
     "Trabalhador de App", 
     "Job Switcher", 
@@ -163,7 +182,8 @@ tab_base, tab_app, tab_switcher, tab_sexo, tab_regioes, tab_carteira, tab_quarti
     "Carteira Assinada",
     "Quartis",
     "Faixa Etária",
-    "Ocupações"
+    "Ocupações",
+    "Divisões ocp."
 ])
 
 # --- Aba 1: Base (Geral) ---
@@ -220,20 +240,22 @@ with tab_sexo:
 
 with tab_regioes:
     st.header("Regiões")
-
+    
+    df_base = load_data(grupos_suffix['Base'] + codigo_deflator)
     df_nordeste = load_data(grupos_suffix['Nordeste'] + codigo_deflator)
     df_sudeste = load_data(grupos_suffix['Sudeste'] + codigo_deflator)
     df_centro_oeste = load_data(grupos_suffix['Centro-Oeste'] + codigo_deflator)
     df_norte = load_data(grupos_suffix['Norte'] + codigo_deflator)
     df_sul = load_data(grupos_suffix['Sul'] + codigo_deflator)
 
+    df_base['Grupo'] = 'Base'
     df_nordeste['Grupo'] = 'Nordeste'
     df_sudeste['Grupo'] = 'Sudeste'
     df_centro_oeste['Grupo'] = 'Centro-Oeste'
     df_sul['Grupo'] = 'Sul'
     df_norte['Grupo'] = 'Norte'
 
-    df_regioes_combined = pd.concat([df_nordeste, df_norte, df_sul, df_sudeste, df_centro_oeste])
+    df_regioes_combined = pd.concat([df_base, df_nordeste, df_norte, df_sul, df_sudeste, df_centro_oeste])
 
     if not df_regioes_combined.empty:
         # Chama a mesma função, mas agora passando 'group_column'
@@ -258,7 +280,7 @@ with tab_carteira:
 
 with tab_quartis:
     st.header("Quartis")
-
+    
     df_base = load_data(grupos_suffix['Base'] + codigo_deflator)
     df_media = load_data(grupos_suffix["Média"] + codigo_deflator)
     df_p25 = load_data(grupos_suffix['Percentil 25'] + codigo_deflator)
@@ -278,7 +300,7 @@ with tab_quartis:
 
 with tab_idade:
     st.header("Faixa Etária")
-
+    
     df_base = load_data(grupos_suffix['Base'] + codigo_deflator)
     df_14_24 = load_data(grupos_suffix['14-24 anos'] + codigo_deflator)
     df_25_54 = load_data(grupos_suffix['25-54 anos'] + codigo_deflator)
@@ -298,22 +320,52 @@ with tab_idade:
 
 with tab_ocp:
     st.header("Grupos de ocupações")
-
+    
     df_base = load_data(grupos_suffix['Base'] + codigo_deflator)
-    df_lm = load_data(grupos_suffix['Lideres e militares'] + codigo_deflator)
-    df_tec_prof = load_data(grupos_suffix['Técnicos e Profissionais'] + codigo_deflator)
-    df_ics = load_data(grupos_suffix['Indústria/Comércio/Serviços'] + codigo_deflator)
-    df_qual = load_data(grupos_suffix['Trabalhadores qualificados'] + codigo_deflator)
-    df_ele = load_data(grupos_suffix['Ocupações Elementares'] + codigo_deflator)
+    df_dir = load_data(grupos_suffix["Diretores e gerentes"] + codigo_deflator)
+    df_int = load_data(grupos_suffix["Prof. das ciências e intelectuais"] + codigo_deflator)
+    df_med = load_data(grupos_suffix["Prof. de nível médio"] + codigo_deflator)
+    df_adm = load_data(grupos_suffix["Trab. de apoio adm."] + codigo_deflator)
+    df_vend = load_data(grupos_suffix["Trab. de serv. e vend."] + codigo_deflator)
+    df_amb = load_data(grupos_suffix["Trab. ambientais qualificados"] + codigo_deflator)
+    df_urb = load_data(grupos_suffix["Trab. urbanos qualificados"] + codigo_deflator)
+    df_ope = load_data(grupos_suffix["Operadores de máquinas"] + codigo_deflator)
+    df_ele = load_data(grupos_suffix["Ocupações elementares"] + codigo_deflator)
+    df_mil = load_data(grupos_suffix["Militares"] + codigo_deflator)
 
     df_base['Grupo'] = "Base"
-    df_lm['Grupo'] = "Lideres"
-    df_tec_prof['Grupo'] = "Profissionais"
-    df_ics['Grupo'] = "Essenciais"
-    df_qual['Grupo'] = "Qualificados"
-    df_ele['Grupo'] = "Elementares"
+    df_dir['Grupo'] = "Diretores e gerentes"
+    df_int['Grupo'] = "Prof. das ciências e intelectuais"
+    df_med['Grupo'] = "Prof. de nível médio"
+    df_adm['Grupo'] = "Trab. de apoio adm."
+    df_vend['Grupo'] = "Trab. de serv. e vend."
+    df_amb['Grupo'] = "Trab. ambientais qualificados"
+    df_urb['Grupo'] = "Trab. urbanos qualificados"
+    df_ope['Grupo'] = "Operadores de máquinas"
+    df_ele['Grupo'] = "Ocupações elementares"
+    df_mil['Grupo'] = "Militares"
     
-    df_ocp_combined = pd.concat([df_base, df_lm, df_tec_prof, df_ics, df_qual, df_ele])
+    df_ocp_combined = pd.concat([df_base, df_dir, df_int, df_med, df_adm, df_vend, df_amb, df_urb, df_ope, df_ele, df_mil])
+
+    if not df_ocp_combined.empty:
+        # Chama a mesma função, mas agora passando 'group_column'
+        chart = create_combined_chart(df_ocp_combined, group_column="Grupo")
+        st.altair_chart(chart, use_container_width=True)
+
+with tab_div:
+    st.header("Grupos de ocupações")
+    
+    df_base = load_data(grupos_suffix['Base'] + codigo_deflator)
+    df_com = load_data(grupos_suffix["Comércios"] + codigo_deflator)
+    df_ser = load_data(grupos_suffix["Serviços"] + codigo_deflator)
+    df_ind = load_data(grupos_suffix["Indústrias"] + codigo_deflator)
+    
+    df_base['Grupo'] = "Base"
+    df_com['Grupo'] = "Comércios"
+    df_ser['Grupo'] = "Serviços"
+    df_ind['Grupo'] = "Indústrias"
+    
+    df_ocp_combined = pd.concat([df_base, df_com, df_ser, df_ind])
 
     if not df_ocp_combined.empty:
         # Chama a mesma função, mas agora passando 'group_column'
